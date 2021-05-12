322e58d44fca90b6d62cb86476594a31ceb94341
itsudhai <itsudhai@yahoo.com>
2021-05-13 01:56:54 +0530

    added to A branch

--- src/org/Sample.java
+++ src/org/Sample.java
@@ -1,6 +1,11 @@
 package org;
 
 public class Sample {
-	String s ="Welcome to GIT";
-
+	public static void main(String[] args) {
+		String s ="Welcome to GIT";
+		System.out.println(s);
+		System.out.println(s.length());
+		System.out.println(s.charAt(2));
+	}
+	
 }
